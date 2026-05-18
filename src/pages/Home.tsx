d9057980a925511e0d79d5c93c2637aa5a01f725
cafagnarob <cafagna.rob@gmail.com>
2026-05-18 11:22:00 +0200

    sidebarstart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
 import { Col, Container, Row } from "react-bootstrap";
+import SidebarStart from "../component_caf/sidebarstart";
 
 const Home = () => {
   return (
     <Container>
       <Row>
         {/* colonna sinistra */}
-        <Col md={3}></Col>
+        <Col md={3}>
+        <SidebarStart/>
+        </Col>
         {/* colonna centrale */}
-        <Col md={6}></Col>
+        <Col md={6}>
+        </Col>
         {/* colonna destra */}
         <Col md={3}></Col>
       </Row>
