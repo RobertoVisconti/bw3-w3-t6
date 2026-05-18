8f1dd97c70b29992c1b73d628f2459e8b0807b73
Roberto Visconti <robertovisconti93@gmail.com>
2026-05-18 12:13:22 +0200

    initial fetch

---
+++ src/redux/actions/index.ts
@@ -0,0 +1,8 @@
+
+
+const getProfile =  'https://striveschool-api.herokuapp.com/api/profile/'
+const getMyProfile = 'https://striveschool-api.herokuapp.com/api/profile/me'
+const getSpecProfile = 'https://striveschool-api.herokuapp.com/api/profile/:userId'
+const putProfile = 'https://striveschool-api.herokuapp.com/api/profile/'
+
+const key = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2YTBhZTM2YjA2YmJlOTAwMTVkZWU1ODQiLCJpYXQiOjE3NzkwOTg0NzUsImV4cCI6MTc4MDMwODA3NX0.-amWlkvhLkyvFEa7toH1UhJxcWNFuvFda6biS6swSVA'
