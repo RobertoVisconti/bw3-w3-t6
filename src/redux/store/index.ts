e77b54ea268f2e1333992137bc8c40d020753914
Roberto Visconti <robertovisconti93@gmail.com>
2026-05-19 11:54:11 +0200

    end fetch with interface

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { profileReducer } from '../reducers/profileReducer';
 import { experienceReducer } from '../reducers/experienceReducer';
 import { postReducer } from '../reducers/postReducer';
+import { commentsReducer } from "../reducers/commentsReducer";
 
 const mainReducer = combineReducers({
   profile: profileReducer,
   experience: experienceReducer,
-  post: postReducer
+  post: postReducer,
+  comments: commentsReducer,
 });
 
 export const store = configureStore({
