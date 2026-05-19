4cfe3d9c7a684eae72006819e797877fb1f94476
Daniele Bulgaru <daniele.bulgaru10@gmail.com>
2026-05-19 12:48:10 +0200

    add SidebarDX Profilo

--- src/pages/Profilo.tsx
+++ src/pages/Profilo.tsx
@@ -1,19 +1,23 @@
-import { Col, Container, Row } from "react-bootstrap"
-
-import MainProfile from "../components/MainProfile"
+import { Col, Container, Row } from "react-bootstrap";
+import ProfileRightSidebar from "../components/ProfileRightSidebar";
+import MainProfile from "../components/MainProfile";
 
 const Profilo = () => {
   return (
-    <Container>
+    <Container className="mt-4">
       <Row>
-        {/* colonna sinistra */}
+        {/* colonna principale */}
         <Col md={9}>
           <MainProfile />
         </Col>
-        {/* colonna centrale */}
-        <Col md={3}></Col>
+
+        {/* sidebar destra */}
+        <Col md={3} className="mt-2">
+          <ProfileRightSidebar />
+        </Col>
       </Row>
     </Container>
-  )
-}
-export default Profilo
+  );
+};
+
+export default Profilo;
