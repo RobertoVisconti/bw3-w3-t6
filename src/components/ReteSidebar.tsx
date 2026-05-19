56ef59a764e5d1688305eacf84aa3b45613ada17
Vidied <davidepan2001@gmail.com>
2026-05-19 12:41:30 +0200

    sidebar rete

--- src/components/ReteSidebar.tsx
+++ src/components/ReteSidebar.tsx
@@ -1,3 +1,6 @@
+import { Button } from "react-bootstrap";
+import { FooterMiniGenerale } from "./FooterMiniGenerale";
+
 interface LinkFormat {
   icon: React.ReactNode;
   label: string;
@@ -38,23 +41,63 @@ export const ReteSidebar = () => {
   ];
 
   return (
-    <section>
-      <article>
-        <h6>Gestsci la tua rete</h6>
-        <hr />
-        <div className="d-flex flex-column gap-3 ">
+    <section className="">
+      <article className="bg-light border border-secondary my-2 pb-2 rounded-2">
+        <h6 className="mx-n4 w-auto border-bottom border-secondary-subtle py-3 px-4">
+          Gestsci la tua rete
+        </h6>
+
+        <div className="d-flex flex-column gap-3 px-4">
           {links.map((link, i) => (
             <a
               key={i}
               href={link.url}
               className="text-decoration-none text-secondary"
             >
-              <span>{link.icon}</span> <span>{link.label}</span>
+              <span className="me-1 fs-5">{link.icon}</span>{" "}
+              <span className="fw-medium">{link.label}</span>
             </a>
           ))}
         </div>
       </article>
-      <article></article>
+      <article className="border border-secondary">
+        <div className="position-relative">
+          {" "}
+          <img
+            src="https://placecats.com/100/50"
+            alt=""
+            className="w-100"
+            style={{ height: "50px" }}
+          />
+          <Button
+            size="sm"
+            className="bg-light text-secondary border-0 position-absolute top-0 end-0 m-1 rounded-3"
+          >
+            Promosso <i className="fas fa-ellipsis-h"></i>
+          </Button>
+        </div>
+        <div
+          className="justify-content-center gap-3 d-flex position-relative"
+          style={{ marginTop: "-10px", zIndex: "3" }}
+        >
+          <img
+            src="https://placecats.com/50/50"
+            alt=""
+            className="rounded-circle"
+          />
+          <img src="https://placecats.com/50/50" alt="" />
+        </div>
+        <div className="p-3 text-center">
+          <h6>Lorem ipsum dolor sit amet consectetur.</h6>
+          <p className="text-muted small">
+            Scopri le ultime offerte di lavore e notizie
+          </p>
+          <Button className="text-primary bg-transparent rounded-pill mt-2 fw-medium border-1 w-100">
+            Segui
+          </Button>
+        </div>
+      </article>
+      <FooterMiniGenerale />
     </section>
   );
 };
--- src/pages/Rete.tsx
+++ src/pages/Rete.tsx
@@ -3,13 +3,16 @@ import { ReteSidebar } from "../components/ReteSidebar";
 
 export const Rete = () => {
   return (
-    <Container>
-      <Row>
+    <Container fluid="md" className="px-0  mt-sm-4">
+      <Row className="g-0 g-sm-3">
         {/* sidebar sx */}
-        <Col xs={12} md={3}></Col>
-        <ReteSidebar></ReteSidebar>
+        <Col xs={12} md={4} lg={3}>
+          {" "}
+          <ReteSidebar></ReteSidebar>
+        </Col>
+
         {/* main rete */}
-        <Col xs={12} md={9}></Col>
+        <Col xs={12} md={8} lg={9}></Col>
       </Row>
     </Container>
   );
